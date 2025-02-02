41de3f2e100d475004d30d7145b65de7c614915b
Evgeniy.Chukreev <dechukor@gmail.com>
2025-02-02 21:37:52 +0500

    Выполнен перенос списка задач в LocalStorage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,11 +3,25 @@ import { ThemeProvider } from "@mui/material/styles";
 import { tasksData } from "../data.ts";
 import { theme } from "./theme.ts";
 import { TodoPage } from "../views";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TaskType } from "../types/types.ts";
 
 export const App = () => {
-  const [tasks, setTasks] = useState<TaskType[]>(tasksData);
+  const [tasks, setTasks] = useState<TaskType[]>(
+    JSON.parse(localStorage.getItem("todoList")!)
+      ? JSON.parse(localStorage.getItem("todoList")!).map((task: TaskType) => {
+          task.dateCreate = task.dateCreate ? new Date(task.dateCreate) : null;
+          task.dateDeadline = task.dateDeadline
+            ? new Date(task.dateDeadline)
+            : null;
+          return task;
+        })
+      : tasksData
+  );
+
+  useEffect(() => {
+    localStorage.setItem("todoList", JSON.stringify(tasks));
+  }, [tasks]);
 
   return (
     <ThemeProvider theme={theme}>
